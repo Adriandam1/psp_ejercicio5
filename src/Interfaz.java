6682c5d5aca9c1d30b1e1e28755abccd28d0c54b
Adriamdam1 <aabeijoncarbajo@danielcastelao.org>
2024-09-26 10:56:09 +0200

    subo

---
+++ src/Interfaz.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Interfaz {
+    public static void main(String[] args) {
+        //BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Indique comando a utilizar: ");
+        String comando = sc.nextLine();
+
+
+
+
+
+    }
+}
---
+++ src/ejecutor.java
@@ -0,0 +1,14 @@
+import javax.xml.stream.events.StartElement;
+
+public class ejecutor {
+
+
+    ProcessBuilder pBlock = new ProcessBuilder("", "");
+    Process p = pBlock();
+
+    private Process pBlock() {
+        return null;
+    }
+
+
+}
